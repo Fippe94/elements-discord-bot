14c5e161cf71a69c103def980aeacdbbb7dcec32
Alan Beam <alan@alanbeam.net>
2018-03-21 11:20:28 -0500

    Add Discord ID to link.js

--- commands/link.js
+++ commands/link.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
 	constructor() {
 		super({
 			name: 'link',
-			desc: 'Link to a forum profile',
+			desc: 'Link to a forum profile. Use via DM.',
 			usage: '<prefix>link <forum_username>',
 		});
 
@@ -16,15 +16,22 @@ module.exports = class extends Command {
 
 	action(message, args) {
 		// Save to database & send forum PM to user
-        utils.post('code.php', { forum_username: args[0] }, body => {
-            if (body == 'success') {
-                // Respond with confirmation
-                message.reply('A Forum PM has been sent to the specified user. Respond with `!verify <code>` using the code from the Forum PM to link your Discord account with your Forum Profile.');
-            } else {
-                // Respond with error
-                console.log(body);
-                message.reply('Something went wrong. Please check the username you entered and try again.');
+        utils.post(
+            'code.php', 
+            { 
+                discord_id: message.author.id,
+                forum_username: args[0] 
+            }, 
+            body => {
+                if (body == 'success') {
+                    // Respond with confirmation
+                    message.reply('A Forum PM has been sent to the specified user. Respond with `!verify <code>` using the code from the Forum PM to link your Discord account with your Forum Profile.');
+                } else {
+                    // Respond with error
+                    console.log(body);
+                    message.reply('Something went wrong. Please check the username you entered and try again.');
+                }
             }
-        });
+        );
 	}
 };
